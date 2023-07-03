1734be59e81ecb59b68a5d6f22c7ea9d5c4c1312
Chamin Jayakody <36810785+chaminakila@users.noreply.github.com>
2023-07-03 18:31:17 +0530

    Create Install_filegator.sh

---
+++ Install_filegator.sh
@@ -0,0 +1,11 @@
+sudo docker run -d \
+  --name=filegator \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=Asia/Colombo \
+  -p 80:80 \
+  -p 443:443 \
+  -v /home/ubuntu/docker/filegator/config:/config \
+  -v /home/ubuntu/docker/qBittorrent/downloads:/data \
+  --restart unless-stopped \
+  maxime1907/filegator
