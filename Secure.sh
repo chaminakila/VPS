cd5ffbca188079fba178cd1f5b2537f6d1b05e58
Chamin Jayakody <36810785+chaminakila@users.noreply.github.com>
2023-07-20 12:45:59 +0530

    Update Secure.sh

--- Secure.sh
+++ Secure.sh
@@ -4,3 +4,5 @@ sudo useradd -s /bin/bash -d /home/kachaya/ -m -G sudo kachaya
 sudo passwd kachaya
 sudo userdel -r ubuntu
 sed -i "s/#Port 22/Port 168/gi" /etc/ssh/sshd_config
+sudo ufw allow 168/tcp
+sudo service ssh restart
