a2d49b026ec44d3e9aa7a2b03a59c71a360ad7f9
Chamin Jayakody <36810785+chaminakila@users.noreply.github.com>
2023-07-23 01:06:20 +0530

    Update Secure.sh

--- Secure.sh
+++ Secure.sh
@@ -1,4 +1,5 @@
-sudo apt update
+sudo apt-get update
+sudo apt-get upgrade
 #https://www.cyberciti.biz/faq/create-a-user-account-on-ubuntu-linux/
 sudo useradd -s /bin/bash -d /home/kachaya/ -m -G sudo kachaya
 sudo passwd kachaya
@@ -9,3 +10,10 @@ sudo ufw allow 168/tcp
 sudo ufw enable
 sudo service ssh restart
 # /etc/services
+
+#https://support.us.ovhcloud.com/hc/en-us/articles/4412351365139-How-to-Secure-a-VPS
+#https://coderrocketfuel.com/article/remove-a-users-sudo-privileges-on-ubuntu
+
+#https://linuxhandbook.com/remove-lines-file/
+#https://www.tutorialspoint.com/run-a-script-on-startup-in-linux
+
