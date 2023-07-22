9f68430410753b004b04df0993ebf2a56c6b0ae0
Chamin Jayakody <36810785+chaminakila@users.noreply.github.com>
2023-07-22 23:50:10 +0530

    Update Secure.sh

--- Secure.sh
+++ Secure.sh
@@ -4,7 +4,7 @@ sudo useradd -s /bin/bash -d /home/kachaya/ -m -G sudo kachaya
 sudo passwd kachaya
 sudo userdel -r ubuntu
 sed -i "s/#Port 22/Port 168/gi" /etc/ssh/sshd_config
-sudo ufw default allow outgoing
-sudo ufw default deny incoming
 sudo ufw allow 168/tcp
-sudo ufw enable -y
+sudo ufw enable
+sudo service ssh restart
+# /etc/services
