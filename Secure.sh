289378f82b9bbaebb68e8d434d24bbc6b54eef8d
Chamin Jayakody <36810785+chaminakila@users.noreply.github.com>
2023-07-23 09:47:20 +0530

    Update Secure.sh

--- Secure.sh
+++ Secure.sh
@@ -1,18 +1,26 @@
 sudo apt-get update
-sudo apt-get upgrade
 #https://www.cyberciti.biz/faq/create-a-user-account-on-ubuntu-linux/
 sudo useradd -s /bin/bash -d /home/kachaya/ -m -G sudo kachaya
 sudo passwd kachaya
-sudo userdel -r ubuntu
 sudo sed -i "s/#Port 22/Port 168/gi" /etc/ssh/sshd_config
+#https://unix.stackexchange.com/questions/145299/simple-sed-replacement-of-tabs-mysteriously-failing
+sudo sed -i "s/$(printf 'ssh\t\t22')/$(printf 'ssh\t\t168')/gi" /etc/services
 #https://askubuntu.com/questions/1439461/ssh-default-port-not-changing-ubuntu-22-10
+mkdir -p /etc/systemd/system/ssh.socket.d
+echo '[Socket]' | sudo tee -a /etc/systemd/system/ssh.socket.d/listen.conf
+echo 'ListenStream=' | sudo tee -a /etc/systemd/system/ssh.socket.d/listen.conf
+echo 'ListenStream=168' | sudo tee -a /etc/systemd/system/ssh.socket.d/listen.conf
+sudo systemctl daemon-reload
+sudo systemctl restart ssh
 sudo ufw allow 168/tcp
 sudo ufw enable
-sudo service ssh restart
+sed -i '2,17d' Secure.sh
+sudo reboot
 # /etc/services
 
 #https://support.us.ovhcloud.com/hc/en-us/articles/4412351365139-How-to-Secure-a-VPS
 #https://coderrocketfuel.com/article/remove-a-users-sudo-privileges-on-ubuntu
+#sudo userdel -r ubuntu
 
 #https://linuxhandbook.com/remove-lines-file/
 #https://www.tutorialspoint.com/run-a-script-on-startup-in-linux
