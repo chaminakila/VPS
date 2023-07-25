cfb3888bae5671179b793ed53356789d0a35455b
Chamin Jayakody <36810785+chaminakila@users.noreply.github.com>
2023-07-25 21:55:28 +0530

    Update Install_qBittorrent.sh

--- Install_qBittorrent.sh
+++ Install_qBittorrent.sh
@@ -11,7 +11,7 @@ sudo docker run -d \
   -p 8080:8080 \
   -p 6881:6881 \
   -p 6881:6881/udp \
-  -v /home/ubuntu/docker/qBittorrent/config:/config \
-  -v /home/ubuntu/docker/qBittorrent/downloads:/downloads \
+  -v /home/kachaya/docker/qBittorrent/config:/config \
+  -v /home/kachaya/docker/qBittorrent/downloads:/downloads \
   --restart unless-stopped \
   lscr.io/linuxserver/qbittorrent:latest
